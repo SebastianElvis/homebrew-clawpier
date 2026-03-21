3603f8fd5f05602ce920695b8c6aeae54d120ebf
Runchao Han <me@runchao.rocks>
2026-03-21 14:40:57 +0800

    Fix SHA256 checksums for v0.3.0-rc2

--- Casks/clawpier.rb
+++ Casks/clawpier.rb
@@ -1,14 +1,14 @@
 cask "clawpier" do
-  version "0.3.0"
+  version "0.3.0-rc2"
 
   on_arm do
     url "https://github.com/SebastianElvis/clawpier/releases/download/v#{version}/ClawPier_v#{version}_aarch64.dmg"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    sha256 "07a420968ea70399d3f6cca09b3ba18baa7496d7b7d9c6bb252da6731375ca9e"
   end
 
   on_intel do
     url "https://github.com/SebastianElvis/clawpier/releases/download/v#{version}/ClawPier_v#{version}_x64.dmg"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    sha256 "b4a4e0f513142362a2f2e58e17dd1dd3501f7e6082dc1525b913da0e7bcb018e"
   end
 
   name "ClawPier"
